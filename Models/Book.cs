af29a34588f2a189978da91b711f257e3202e562
Dan Cowdry <medtc411@gmail.com>
2021-12-21 11:54:25 -0800

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BookCollection.Models
+{
+    public class Book
+    {
+        public string BookTitle { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPages { get; set; }
+        public int Id { get; }
+        private static int nextId = 1;
+
+        public Book()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Book(string booktitle, string authorfirstname, string authorlastname, string genre, int numberofpages)
+        {
+            BookTitle = booktitle;
+            AuthorFirstName = authorfirstname;
+            AuthorLastName = authorlastname;
+            Genre = genre;
+            NumberOfPages = numberofpages;
+        }      
+
+        public override string ToString()
+        {
+            return BookTitle;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Book book &&
+                   Id == book.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
