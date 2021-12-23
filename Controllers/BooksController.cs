47dca6bd5accf1b30280e0ba670d66ecf3753668
Dan Cowdry <medtc411@gmail.com>
2021-12-22 19:29:37 -0800

    Finished Book Controller, able to add books now.

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookCollection.Data;
+using BookCollection.Models;
+using BookCollection.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
 
 namespace BookCollection.Controllers
 {
@@ -6,7 +11,48 @@ namespace BookCollection.Controllers
     {
         public IActionResult Index()
         {
+            List<Book> books = new List<Book>(BookData.GetAll());
+            return View(books);
+        }
+
+        public IActionResult Add()
+        {
+            AddBookViewModel addBookViewModel = new AddBookViewModel();
+            return View(addBookViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddBookViewModel addBookViewModel)
+        {
+            Book newBook = new Book
+            {
+                BookTitle = addBookViewModel.BookTitle,
+                AuthorFirstName = addBookViewModel.AuthorFirstName,
+                AuthorLastName = addBookViewModel.AuthorLastName,
+                Genre = addBookViewModel.Genre,
+                NumberOfPages = addBookViewModel.NumberOfPages,
+            };
+
+            BookData.Add(newBook);
+
+            return Redirect("/Books");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.books = BookData.GetAll();
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] bookIds)
+        {
+            foreach (int bookId in bookIds)
+            {
+                BookData.Remove(bookId);
+            }
+
+            return Redirect("/Books");
         }
     }
 }
