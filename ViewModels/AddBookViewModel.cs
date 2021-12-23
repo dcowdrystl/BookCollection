d33ddd98960e5164d3fa4bedff4f5b6518326d41
Dan Cowdry <medtc411@gmail.com>
2021-12-22 19:13:06 -0800

    worked on view/books/add

---
+++ ViewModels/AddBookViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCollection.ViewModels
+{
+    public class AddBookViewModel
+    {
+        public string BookTitle { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+}
