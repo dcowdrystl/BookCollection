6b3efb0ddee6998ccc98f9f1083a948025e5f23a
Dan Cowdry <medtc411@gmail.com>
2021-12-31 14:21:29 -0800

    added Date to empty User constructor in User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,16 @@
-﻿namespace BookCollection.Models
+﻿using System;
+
+namespace BookCollection.Models
 {
     public class User
     {
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
+        public DateTime Date { get;  }
         public User()
         {
+            Date = DateTime.Now;
         }
 
         public User(string u, string e, string p)
