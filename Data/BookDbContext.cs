96e0f4b97e65109619c2d7428762b156d86b9cb7
Dan Cowdry <medtc411@gmail.com>
2022-01-10 12:39:07 -0800

    able to register

--- Data/BookDbContext.cs
+++ Data/BookDbContext.cs
@@ -1,14 +1,21 @@
 ﻿using BookCollection.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookCollection.Data
 {
-    public class BookDbContext : DbContext
+    public class BookDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Book> Books { get; set; }
 
         public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
         { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- Migrations/20220110203706_InitialMigration.Designer.cs
+++ Migrations/20220110203706_InitialMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BookCollection.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookCollection.Migrations
 {
     [DbContext(typeof(BookDbContext))]
-    partial class BookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220110203706_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
