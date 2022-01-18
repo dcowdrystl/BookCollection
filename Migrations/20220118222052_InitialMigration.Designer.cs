e3fdcba3e6d083793d421e81e09fc00443081672
Dan Cowdry <medtc411@gmail.com>
2022-01-18 14:54:28 -0800

    added viewall

--- Migrations/20220118222052_InitialMigration.Designer.cs
+++ Migrations/20220118222052_InitialMigration.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookCollection.Migrations
 {
     [DbContext(typeof(BookDbContext))]
-    [Migration("20220118201606_InitialMigration")]
+    [Migration("20220118222052_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
